a2f8b75d990dd2a527e150b53f1e1c44c5b5ea0f
Luis Emanuel Hernandez Flores <58230791+luis760@users.noreply.github.com>
2023-03-02 21:12:25 -0600

    Add files via upload

---
+++ _1_Variables.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar  2 20:38:39 2023
+
+@author: 52331
+"""
+
+#librerias
+import numpy as np
+import cv2
+#variable de tipo cadena de texto
+mensaje = "Esta es mi primera practica de la materia de inteligencia artificial del semestre: "
+#variable de tipo caracter
+semestre=6
+#Mostramos el mensaje
+print(mensaje, semestre )
